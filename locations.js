36f03488a723e0634e65eb45fc14319565234c89
bonirl71 <bonirl71@gmail.com>
2026-07-26 21:12:19 +0100

    Add swimLocations array with location data
    Add swim locations database

---
+++ locations.js
@@ -0,0 +1,26 @@
+const swimLocations = [
+    {
+        id: 1,
+        name: "Blackhead Lighthouse → Gleninagh Pier",
+        county: "Clare",
+        latitude: 53.148,
+        longitude: -9.265,
+        distance: "2.5 km"
+    },
+    {
+        id: 2,
+        name: "Fanore Beach",
+        county: "Clare",
+        latitude: 53.125,
+        longitude: -9.290,
+        distance: "TBC"
+    },
+    {
+        id: 3,
+        name: "Lahinch Beach",
+        county: "Clare",
+        latitude: 53.053,
+        longitude: -9.344,
+        distance: "TBC"
+    }
+];
